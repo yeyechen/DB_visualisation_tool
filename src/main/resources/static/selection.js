c653a17f83f74f0aa660463eefc51a00e50a71ed
yeyechen <yc1319@imperial.ac.uk>
2023-04-04 11:54:25 +0100

    introduce GUI for user selection stage; capture selection by post; needs general refactor

--- src/main/resources/static/selection.js
+++ src/main/resources/static/selection.js
@@ -1,11 +1,26 @@
 $(document).ready(function() {
-  $.ajax({
-    url: '/mondial/options',
-    success: function(options) {
-      var optionSelect = $('#option-select');
-      $.each(options, function(index, option) {
-        optionSelect.append($('<option></option>').val(option).text(option));
+  $.get("mondial/options", function(tableData) {
+    // Generate the checkboxes for each table
+    $.each(tableData, function(index, table) {
+      var $tableListItem = $("<li></li>").appendTo("#table-list");
+      $("<h3>" + table.name + "</h3>").appendTo($tableListItem);
+      $.each(table.attributes, function(index, attribute) {
+        $("<label><input type='checkbox' name='" + table.name + "-" + attribute + "' value='" + table.name + "." + attribute + "'> " + attribute + "</label><br>").appendTo($tableListItem);
       });
-    }
+    });
+  });
+
+  $("#attribute-form").submit(function(event) {
+    event.preventDefault();
+
+    // Get the selected checkboxes
+    var selectedAttributes = [];
+    $("input:checked").each(function() {
+      selectedAttributes.push($(this).val());
+    });
+
+    $.post("mondial/selection", { attributes: JSON.stringify(selectedAttributes) }, function(data) {
+      alert("You selected: " + data);
+    });
   });
 });
