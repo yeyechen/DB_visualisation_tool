cc86fb892db23f87069f34015cfe50a546984448
yeyechen <yc1319@imperial.ac.uk>
2023-04-11 16:41:26 +0100

    limit user selection up to 4 attributes in only one certain table

--- src/main/resources/static/selection.js
+++ src/main/resources/static/selection.js
@@ -1,14 +1,56 @@
 $(document).ready(function() {
+
+  // Disable all checkboxes except for the ones in the clicked table
+  function disableCheckboxes(table) {
+    $("input[type='checkbox']").not("input[name^='" + table + "']").attr("disabled", true);
+  }
+
+  // Disable all checkboxes if the maximum number of checkboxes is checked
+  function disableAllCheckboxes() {
+    $("input[type='checkbox']").not(":checked").attr("disabled", true);
+  }
+
+  // Enable checkboxes in a certain table
+  function enableCheckboxes(table) {
+    $("input[type='checkbox'][name^='" + table + "']").attr("disabled", false);
+  }
+
+  // Enable all checkboxes in all tables
+  function enableAllCheckboxes() {
+    $("input[type='checkbox']").attr("disabled", false);
+  }
+
+  // Count the number of selected checkboxes in a table
+  function countSelectedCheckboxes(table) {
+    return $("input[name^='" + table + "']:checked").length;
+  }
+
   $.get("/options", function(tableData) {
     // Generate the checkboxes for each table
     $.each(tableData, function(index, table) {
       var $tableListItem = $("<tr></tr>").appendTo("#table-list tbody");
       $("<td>" + table.name + "</td>").appendTo($tableListItem);
-      $("<td><strong>" + table.pKey + "</strong></td>").appendTo($tableListItem);
+      $("<td><u>" + table.pKey + "</u></td>").appendTo($tableListItem);
       var $attributesTd = $("<td></td>").appendTo($tableListItem);
       $.each(table.attributes, function(index, attribute) {
         $("<label><input type='checkbox' name='" + table.name + "-" + attribute + "' value='" + table.name + "." + attribute + "'> " + attribute + "</label><br>").appendTo($attributesTd);
       });
+    });
+
+    // Disable checkboxes in other tables when a checkbox is clicked
+    $("input[type='checkbox']").click(function() {
+      var table = $(this).attr("name").split("-")[0];
+      disableCheckboxes(table);
+
+      if (countSelectedCheckboxes(table) >= 4) {
+        disableAllCheckboxes();
+      }
+      if (countSelectedCheckboxes(table) < 4) {
+        enableCheckboxes(table);
+      }
+      if (countSelectedCheckboxes(table) === 0) {
+        enableAllCheckboxes();
+      }
     });
   });
 
