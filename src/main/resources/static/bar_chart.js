172735bbda8b181e28f9411ff2a70c2a92bdbfc9
yeyechen <yc1319@imperial.ac.uk>
2023-04-25 22:35:05 +0100

    refine bar chart; now support zooming in/out

--- src/main/resources/static/bar_chart.js
+++ src/main/resources/static/bar_chart.js
@@ -1,55 +1,84 @@
-import { margin, width, height, createSvg } from "./common.js";
-const svg = createSvg();
+function BarChart(data, {
+  margin = ({top: 20, right: 0, bottom: 30, left: 100}),
+  width = 870,
+  height = 500,
+  keys = Object.keys(data[0]), // index: 0->k, 1->a1
+  yLabel = keys[1]
+} = {}) {
 
-// Load the data from the server
-d3.json("/bar_chart_data")
-  .then(function(data) {
+  x = d3.scaleBand()
+      .domain(data.map(d => d[keys[0]]))
+      .range([margin.left, width - margin.right])
+      .padding(0.1);
 
-    // Extract the primary key name and attribute name (x-axis data and y-axis data)
-    var keys = Object.keys(data[0]);
+  y = d3.scaleLinear()
+      .domain([0, d3.max(data, d => d[keys[1]])]).nice()
+      .range([height - margin.bottom, margin.top]);
 
-    // display attribute value in descending order
-    data.sort((a, b) => (b[keys[1]] - a[keys[1]]));
+  xAxis = g => g
+      .attr("transform", `translate(0,${height - margin.bottom})`)
+      .call(d3.axisBottom(x).tickSizeOuter(0));
 
-    // todo: let user decide
-    var topN = 10;
-    // first n result
-    data = data.slice(0, topN);
+  yAxis = g => g
+      .attr("transform", `translate(${margin.left},0)`)
+      .call(d3.axisLeft(y))
+      .call(g => g.select(".domain").remove())
+      .append("text")
+        .attr("fill", "black")
+        .attr("x", -margin.left)
+        .attr("y", margin.top / 2)
+        .attr("text-anchor", "start")
+        .text(yLabel);
 
-    var x = d3.scaleBand()
-      .range([0, width])
-      .padding(0.1)
-      .domain(data.map(d => d[keys[0]]));
+  const svg = d3.create("svg")
+      .attr("viewBox", [0, 0, width, height])
+      .call(zoom);
 
-    var y = d3.scaleLinear()
-      .range([height, 0])
-      .domain([0, d3.max(data, d => d[keys[1]])]);
+  svg.append("g")
+      .attr("class", "bars")
+      .attr("fill", "steelblue")
+    .selectAll("rect")
+    .data(data)
+    .join("rect")
+      .attr("x", d => x(d[keys[0]]))
+      .attr("y", d => y(d[keys[1]]))
+      .attr("height", d => y(0) - y(d[keys[1]]))
+      .attr("width", x.bandwidth());
 
-    var xAxis = d3.axisBottom(x);
-
-    var yAxis = d3.axisLeft(y)
-      .ticks(10)
-      .tickFormat(d3.format(".2s"));
-
-    svg.append("g")
+  svg.append("g")
       .attr("class", "x-axis")
-      .attr("transform", "translate(0," + height + ")")
       .call(xAxis);
 
-    svg.append("g")
+  svg.append("g")
       .attr("class", "y-axis")
       .call(yAxis);
 
-    svg.selectAll(".bar")
-      .data(data)
-      .enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", d => x(d[keys[0]]))
-      .attr("width", x.bandwidth())
-      .attr("y", d => y(d[keys[1]]))
-      .attr("height", d => height - y(d[keys[1]]))
-      .attr("fill", "steelblue");
-  });
+  function zoom(svg) {
+    const extent = [[margin.left, margin.top], [width - margin.right, height - margin.top]];
+
+    svg.call(d3.zoom()
+        .scaleExtent([1, 8])
+        .translateExtent(extent)
+        .extent(extent)
+        .on("zoom", zoomed));
+
+    function zoomed(event) {
+      x.range([margin.left, width - margin.right].map(d => event.transform.applyX(d)));
+      svg.selectAll(".bars rect").attr("x", d => x(d[keys[0]])).attr("width", x.bandwidth());
+      svg.selectAll(".x-axis").call(xAxis);
+    }
+  }
+
+  return svg.node();
+}
+
+d3.json("/bar_chart_data")
+  .then(function(data) {
+
+  const svg = BarChart(data);
+  d3.select("body").append(() => svg);
+
+});
 
 
 
